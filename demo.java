6720df283ce849946fa03850b5a59ae85091407f
Samardhan <kampallysamardhan23@gmail.com>
2023-04-12 18:15:41 +0530

    add div

--- demo.java
+++ demo.java
@@ -8,17 +8,10 @@ int d=a+b+c;
 System.out.println(d);
 
 
-<<<<<<< Updated upstream
 int p=20;
 int q=10;
 int r=p/q;
-=======
 
-int x=2;
-int y=3;
-//incomplete code
-
->>>>>>> Stashed changes
 
 }
 }
